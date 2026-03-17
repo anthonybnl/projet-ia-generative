07129c7bc9d3d90bb2b5a5089c96d9748e21ac27
anthonybnl <blondel.anthony@gmail.com>
2026-03-17 12:04:01 +0100

    📝 referentiel : ajout de la description métier

--- src/nettoyage_donnees.py
+++ src/nettoyage_donnees.py
@@ -10,15 +10,19 @@ from chromadb import (
     Embeddings,
     HttpClient as ChromaHttpClient,
 )
+from pathlib import Path
 
 # chargement des CSV
-csv_path_base = os.path.join(os.getcwd(), "poc-referentiel-competence")
+csv_path_base = Path.cwd() / "poc-referentiel-competence"
 
-df_metier = pd.read_csv(os.path.join(csv_path_base, "cigref.csv"), sep=";")
-df_competence = pd.read_csv(
-    os.path.join(csv_path_base, "cigref compétence.csv"), sep=";"
+df_metier = pd.read_csv(csv_path_base / "cigref.csv", sep=";")
+df_competence = pd.read_csv(csv_path_base / "cigref compétence.csv", sep=";")
+
+df_descriptions_metiers = pd.read_csv(
+    csv_path_base / "descriptions_metiers.csv", sep=",", quotechar='"'
 )
 
+print(df_descriptions_metiers[["métier"]].head())
 # nettoyage des données
 
 df_competence["id"] = df_competence["compétence_id"].apply(
@@ -42,10 +46,14 @@ df_competence_clean.to_csv(
 df_metier = df_metier.merge(
     df_competence, how="left", left_on="compétence_id", right_on="compétence_id"
 )
-
-df_metier_clean = df_metier[["profil métier", "domaine de compétence", "id", "niveau"]].rename(
-    columns={"profil métier": "métier", "id": "id_competence"}
+df_metier = df_metier.merge(
+    df_descriptions_metiers, left_on="profil métier", right_on="métier", how="left"
 )
+
+df_metier_clean = df_metier[
+    ["profil métier", "domaine de compétence", "id", "niveau", "description"]
+].rename(columns={"profil métier": "métier", "id": "id_competence"})
+
 
 df_metier_clean.to_csv(
     os.path.join(os.getcwd(), "data", "cigref_metier_clean.csv"),
